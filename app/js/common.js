ed33ce5b406b74379baf00dcb8053732d60e79f1
Dmitry Toshchev <dterron@yandex.ru>
2018-06-05 11:59:57 +0300

    header+asideNav+.poster done

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	// Making top navbar thinner at scroll
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 0) {
 			$('.header__wrapper').css('padding', '10px 60px');
@@ -7,5 +8,10 @@ $(function() {
 			$('.header__wrapper').css('padding', '20px 60px');
 		}
 	});
+
+	// Show-hide aside menu
+	$('.header__menu').on('click', function() {
+		$('.asideNav').toggleClass('asideNav_visible');
+ 	});
 
 });
